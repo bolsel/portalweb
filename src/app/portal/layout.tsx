4c6c965987484d8ae8ce24fc6a32ffcc818b9751
upiksaleh <upxsal@gmail.com>
2024-10-01 15:21:49 +0800

    initial portal page

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -1,7 +1,19 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <div>{children}</div>;
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Portalweb Bolsel',
+};
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col h-screen">
+      <header className="bg-primary text-white font-intro text-center text-xl p-2">
+        PORTALWEB BOLSEL
+      </header>
+      <main className="flex-1 bg-base-200">{children}</main>
+      <footer className="bg-base-300 text-center py-2">
+        &copy; Dinas Komunikasi dan Informatika Pemkab Bolsel
+      </footer>
+    </div>
+  );
 }
---
+++ src/app/portal/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className="ui-container py-5 text-center">
+      Konten pada halaman ini masih dalam pengembangan.
+    </div>
+  );
+}
