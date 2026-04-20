7d7054fe16384fa083e865d133984124801bac43
upiksaleh <upik@codeup.id>
2026-04-20 13:41:14 +0800

    update GPR widget script URL to use the correct domain

--- src/components/widgets/gpr-widget.tsx
+++ src/components/widgets/gpr-widget.tsx
@@ -5,7 +5,7 @@ import { useRef } from 'react';
 
 export const WidgetGPR = () => {
   const ref = useRef<HTMLDivElement>(null);
-  useScript('https://widget.kominfo.go.id/gpr-widget-kominfo.min.js', ref);
+  useScript('https://widget.komdigi.go.id/gpr-widget-kominfo.min.js', ref);
   return (
     <>
       <div ref={ref}></div>
